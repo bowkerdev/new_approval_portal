03a7b7caaa3bda6930a3c8eeb4dff0bb84a541cf
yee <1062948995@qq.com>
2021-06-24 16:56:08 +0800

    fix(treeSelect): 滚动条影响到其他

--- src/components/treeSelect/treeSelect.vue
+++ src/components/treeSelect/treeSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-select :value="valueTitle" :size="size"  :disabled="disabled" :clearable="clearable" :placeholder="placeholderText" @clear="clearHandle">
+  <el-select class="el-select-scope" :value="valueTitle" :size="size"  :disabled="disabled" :clearable="clearable" :placeholder="placeholderText" @clear="clearHandle">
     <el-option :value="valueTitle"  :label="valueTitle" class="options">
       <el-tree  id="tree-option"
         ref="selectTree"
@@ -162,8 +162,8 @@ export default {
     // 初始化滚动条
     initScroll () {
       this.$nextTick(() => {
-        let scrollWrap = document.querySelectorAll('.el-scrollbar .el-select-dropdown__wrap')[0]
-        let scrollBar = document.querySelectorAll('.el-scrollbar .el-scrollbar__bar')
+        let scrollWrap = document.querySelectorAll('.el-select-scope .el-scrollbar .el-select-dropdown__wrap')[0]
+        let scrollBar = document.querySelectorAll('.el-select-scope .el-scrollbar .el-scrollbar__bar')
         if (scrollWrap) { scrollWrap.style.cssText = 'margin: 0px; max-height: none; overflow: hidden;' }
         if (scrollBar) {
           scrollBar.forEach(ele => {

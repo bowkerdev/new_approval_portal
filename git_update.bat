24f9b29ee0e9706a22f8df3355797faaf676299f
lewis <lewis.liu@gmfzhimi.com>
2021-07-19 16:32:24 +0800

    init

--- git_update.bat
+++ git_update.bat
@@ -1,3 +1,3 @@
 git commit -m "init"
-git subtree pull --prefix=flow_ui https://gitee.com/gmfzhimi/gmf_common_frame.git master
+git subtree pull --prefix=flow-ui https://gitee.com/gmfzhimi/gmf_common_frame.git master
 pause
